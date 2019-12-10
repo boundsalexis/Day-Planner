968add2907593dca2b87d866ffca435eacd0ee60
Alexis <lxs.bnds@gmail.com>
2019-12-10 13:24:07 -0800

    fully functional color coding, on 24 hour clock albeit

--- script.js
+++ script.js
@@ -3,50 +3,43 @@ $(document).ready(function(){
  var time = document.querySelectorAll(".hour");
  var userInput = document.querySelectorAll(".text");
  var save =document.querySelectorAll(".save");
-// var hour = moment().format("h");
+var hour = moment().format("HH");
 
-var hour=3;
+// var hour=3;
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
  let updateTime= function (){
      let currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
      $("#currentDay").text(currentTime);
  }
- console.log(userInput[2]);
- $(".text").addClass("future");
-updateTime();
-setInterval(updateTime, 1000);
 
 function checkColors(){
-    for(i= 0;i < time.length; i++){
-        var actualTime= parseInt(hour);
-        var compare = parseInt(time[i].textContent);        
-        if(compare ===  actualTime){
+    for( var i =0; i < time.length; i ++){
+    var actualTime= parseInt(hour);
+    var timeSlot = parseInt(time[i].textContent);
+    console.log(actualTime);
+    console.log(timeSlot);
+    console.log
+        if(actualTime > timeSlot){
+            $(userInput[i]).addClass("past");
+            $(userInput[i]).removeClass("future");
+            $(userInput[i]).removeClass("present");
+        }
+        else if ( actualTime === timeSlot){
             $(userInput[i]).addClass("present");
-            $(userInput[i]).removeClass("future past");
+            $(userInput[i]).removeClass("future");
+            $(userInput[i]).removeClass("past");
         }
-        else if(i >= 4 && actualTime <compare){
-            console.log("eggs");
-            $(userInput[i]).addClass("past");
-            $(userInput[i]).removeClass("future present")
+        else if (actualTime< timeSlot){
+            $(userInput[i]).addClass("future");
+            $(userInput[i]).removeClass("past");
+            $(userInput[i]).removeClass("present");
+
         }
-        else if (actualTime> compare){
-            $(userInput[i]).addClass("past");
-            $(userInput[i]).removeClass("future present");
-        }
-}}
-checkColors();
-// $(".saveBtn").on("click", function(){
-//     var newEvent = $("textarea").val()
-// })
-// var hour = moment().format("h");
-// console.log(hour);
-// var test = parseInt("7am");
-// console.log(test);
-// console.log("_____", $(".hour").text());
-// console.log(parseInt("7 am"));
-// function checkColors(){
-//     if(parseInt($(".hour").text())===9){
-//         $(".text").addClass("present");
-//     }
-//    }
+}
+}
+
+$(userInput).addClass("future");
+updateTime();
+setInterval(updateTime, 1000);
+setInterval(checkColors, 1000);
 })
