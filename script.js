3f5635b7e27f66bb472d082d5716de21ffc124f6
Alexis <lxs.bnds@gmail.com>
2019-12-10 09:55:52 -0800

    semi functional color coding

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 $(document).ready(function(){
+
+ var time = document.querySelectorAll(".hour");
+ var userInput = document.querySelectorAll(".text");
+ var save =document.querySelectorAll(".save");
+// var hour = moment().format("h");
+
+var hour=3;
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
  let updateTime= function (){
      let currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
      $("#currentDay").text(currentTime);
  }
- var time = document.querySelectorAll(".hour");
- console.log(time);
+ console.log(userInput[2]);
  $(".text").addClass("future");
 updateTime();
 setInterval(updateTime, 1000);
+
+function checkColors(){
+    for(i= 0;i < time.length; i++){
+        var actualTime= parseInt(hour);
+        var compare = parseInt(time[i].textContent);        
+        if(compare ===  actualTime){
+            $(userInput[i]).addClass("present");
+            $(userInput[i]).removeClass("future past");
+        }
+        else if(i >= 4 && actualTime <compare){
+            console.log("eggs");
+            $(userInput[i]).addClass("past");
+            $(userInput[i]).removeClass("future present")
+        }
+        else if (actualTime> compare){
+            $(userInput[i]).addClass("past");
+            $(userInput[i]).removeClass("future present");
+        }
+}}
+checkColors();
+// $(".saveBtn").on("click", function(){
+//     var newEvent = $("textarea").val()
+// })
 // var hour = moment().format("h");
 // console.log(hour);
 // var test = parseInt("7am");
