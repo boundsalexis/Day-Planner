5550e3fe9aa0d51214bdf93d439345be7a387d24
Alexis <lxs.bnds@gmail.com>
2019-12-10 21:49:48 -0800

    readme finished, comments added

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Day-Planner
 Plan your day
+
+# links
+> [GitHub Repository](https://github.com/boundsalexis/Day-Planner)
+> [Live Link](https://boundsalexis.github.io/Day-Planner/)
+
+# Technologies Used
+> HTML to create the physical shell of the planner
+> CSS to style the planner
+> JQUERY for button functionality, the changing of the colors
+> Moment.js for displaying the current time and for the conditionals in respect to changing the colors
+
+# Summary
+Enter your plans for your day and save them so you won't forget. Keep traack of your progress throughout the day.
+
+# Code Snippet
+``` javascript
+function checkColors(){
+    for( var i =0; i < time.length; i ++){
+    var actualTime= parseInt(hour);
+    var timeSlot = parseInt($(time[i]).attr("data-milTime"));
+        if(actualTime > timeSlot){
+            $(userInput[i]).addClass("past");
+            $(userInput[i]).removeClass("future");
+            $(userInput[i]).removeClass("present");
+        }
+        else if ( actualTime === timeSlot){
+            $(userInput[i]).addClass("present");
+            $(userInput[i]).removeClass("future");
+            $(userInput[i]).removeClass("past");
+        }
+        else if (actualTime< timeSlot){
+            $(userInput[i]).addClass("future");
+            $(userInput[i]).removeClass("past");
+            $(userInput[i]).removeClass("present");
+        }
+}
+```
+> This was the hardest part of the project for me, I went through so many combinations of loops and conditional statements before someone in class suggested trying military time. After that it was like a lightbulb went off and the rest of the project was a breeze
+
+## Authors
+
+* **Alexis Bounds** 
+
+- [Link to Portfolio Site](https://github.com/boundsalexis/basic-portfolio)
+- [Link to Github](https://boundsalexis.github.com/)
+- [Link to LinkedIn](https://www.linkedin.com/in/alexis-bounds-9b7711169/)
--- script.js
+++ script.js
@@ -4,28 +4,29 @@ $(document).ready(function(){
  var userInput = document.querySelectorAll(".text");
  var save =document.querySelectorAll(".save");
 var hour = moment().format("HH");
-
+//sets current day on top part of screen
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+// run this ever second for live time
  let updateTime= function (){
-     let currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-     $("#currentDay").text(currentTime);
+     let currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a"); // formats time
+     $("#currentDay").text(currentTime); //pushes to dom
  }
 
 function checkColors(){
     for( var i =0; i < time.length; i ++){
-    var actualTime= parseInt(hour);
-    var timeSlot = parseInt($(time[i]).attr("data-milTime"));
-        if(actualTime > timeSlot){
+    var actualTime= parseInt(hour);// reformat as military time to compare
+    var timeSlot = parseInt($(time[i]).attr("data-milTime")); // use military time to compare
+        if(actualTime > timeSlot){ // for past
             $(userInput[i]).addClass("past");
             $(userInput[i]).removeClass("future");
             $(userInput[i]).removeClass("present");
         }
-        else if ( actualTime === timeSlot){
+        else if ( actualTime === timeSlot){ //for present
             $(userInput[i]).addClass("present");
             $(userInput[i]).removeClass("future");
             $(userInput[i]).removeClass("past");
         }
-        else if (actualTime< timeSlot){
+        else if (actualTime< timeSlot){// for future
             $(userInput[i]).addClass("future");
             $(userInput[i]).removeClass("past");
             $(userInput[i]).removeClass("present");
@@ -34,15 +35,18 @@ function checkColors(){
 }
 $(".saveBtn").on("click", function(event){
     event.preventDefault();
-    var id = "#";
-    id += $(this).attr("data-val");
-    var eventTime =$(this).attr("data-val");
-    var input = $(id).val();
-    window.localStorage.setItem(eventTime, input);
+    var id = "#"; // so that we can use this variable in our Jquery
+    id += $(this).attr("data-val"); 
+    var eventTime =$(this).attr("data-val"); // create eventTime to be able to dynamically load our local storage without the #
+    var input = $(id).val(); // retrieve user input
+    window.localStorage.setItem(eventTime, input); // setting everything to local storage
 })
-
+// sets all time blocks to future
 $(userInput).addClass("future");
+// intializes time
 updateTime();
+//checks to update time every second
 setInterval(updateTime, 1000);
+// checks to see if colors need to be changed
 setInterval(checkColors, 1000);
 })
